305e75346cb751ddae21509ccc035c6e2ab8d548
Gihan Shyavinda Jayathissa <jayathissakdgs.21@uom.lk>
2024-10-04 22:16:57 +0530

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
+  <Header/>
   <Routes>
     <Route path="/about" element={<About />} />
     <Route path="/signin" element={<SignIn />} />
