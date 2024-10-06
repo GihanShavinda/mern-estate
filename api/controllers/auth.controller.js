01d4ccf1c237500690b68fd62b0597b41ea4a0f6
Gihan Shyavinda Jayathissa <jayathissakdgs.21@uom.lk>
2024-10-06 16:39:08 +0530

    ....

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
+export const signup = async (req, res,next) => {
+ const { username, email, password } = req.body;
+ const hashedPassword =bcryptjs.hashSync(password, 10);
+ const newUser = new User({ username, email, password: hashedPassword });
+ try {
+    await newUser.save()
+    res.status(201).json('User created successfully!!!');
+ } catch (error) {
+        next(error);
+ }
+  
+};
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+       const validUser = await User.findOne({ email: email });
+       if (!validUser) {
+          return next(errorHandler(404, 'User not found!'));
+       } 
+       const validPassword = bcryptjs.compareSync(password, validUser.password); 
+         if (!validPassword) {
+             return next(errorHandler(400, 'Wrong credentials!'));
+         }
+       const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+       const { password: hashedPassword, ...user } = validUser._doc;
+       res.cookie('token', token, {httpOnly: true}).status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
